b394bdd07305a49a74f6c94b0ec1c5f36f5910af
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-22 14:34:18 +0100

    list and button

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,17 +7,43 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @StateObject private var expenses = Expenses()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
         }
-        .padding()
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
+
 
 #Preview {
     ContentView()
