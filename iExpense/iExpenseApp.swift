85fe8299cf2afa2649b8cfaf512d42dca737ea69
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-22 14:11:23 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by charlene hoareau on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
